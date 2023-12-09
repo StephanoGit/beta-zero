5ba2d4c781fa031b126feaec8647d8fbb74be1df
Aaron Panaitescu <aaronpanaitescu@gmail.com>
2023-12-09 19:47:45 +0000

    Update game to use DFS for win condition.

--- game.py
+++ game.py
@@ -21,45 +21,41 @@ class Hex:
 
         r = action // self.size
         c = action % self.size
-        value = (action + 1) * player
-        state[r, c] = value
-
-        neighbours = self._neighbours(action)
-        for nr, nc in neighbours:
-            if nr < 0:
-                continue
-            if np.sign(state[nr, nc]) != player:
-                continue
-
-            state = np.where(state == state[nr, nc], value, state)
+        state[r, c] = player
 
         return state
-
 
     def check_win(self, state, action):
         if action is None or action == self.action_size - 1:
             return False
 
-        ## Maybe change this
         r = action // self.size
         c = action % self.size
         player = np.sign(state[r, c])
 
         if player == 1:
-            start = state[0, :]
-            end = state[-1, :]
+            condition = lambda a: a // self.size == self.size - 1
         else:
-            start = state[:, 0]
-            end = state[:, -1]
+            condition = lambda a: a % self.size == self.size - 1
 
-        print(start)
-        print(end)
-        print(player)
-        print(start == end)
-        print(np.sign(start) == player)
-        print((start == end) & (np.sign(start) == player))
+        visited = set()
+        def dfs(a):
+            if condition(a):
+                return True
+            if a in visited:
+                return False
+            visited.add(a)
+            for rn, cn in self._neighbours(a):
+                if rn != -1 and state[rn, cn] == player and dfs(rn * self.size + cn):
+                    return True
+            return False
 
-        return np.any((start == end) & (np.sign(start) == player))
+        end = range(self.size) if player == 1 else range(0, self.action_size - 1, self.size)
+
+        for a in end:
+            if state[a // self.size, a % self.size] == player and dfs(a):
+                return True
+        return False
 
     def get_value_and_terminated(self, state, action):
         if self.check_win(state, action):
@@ -72,8 +68,17 @@ class Hex:
     def get_opponent_value(self, value):
         return -value
 
+    def get_opponent_action(self, action):
+        if action == self.action_size - 1:
+          return action
+        r, c = action // self.size, action % self.size
+        return c * self.size + r
+
     def change_perspective(self, state):
         # Why should we care what player
+        if len(state.shape) == 3:
+          return np.swapaxes(state, 1, 2) * -1
+        assert len(state.shape) == 2
         return state.T * -1
 
     def get_encoded_state(self, state, player):
@@ -87,7 +92,7 @@ class Hex:
 
         encoded_state = np.stack((my_board, op_board, no_board, can_swap)).astype(np.float32)
 
-        if len(encoded_state.shape) == 3:
+        if len(state.shape) == 3:
             encoded_state = np.swapaxes(encoded_state, 0, 1)
 
         return encoded_state
@@ -116,14 +121,13 @@ class Hex:
         return np.swapaxes(np.where(on_board, (rs, cs), -1), 0, 1)
 
 
-    def pretty_please_state(self, state, blob=False):
+    def pretty_please_state(self, state):
         """Display the current state (nicely)."""
         no_to_symbol = {
                 -1: "O",
                 0: ".",
                 1: "#"
         }
-        # state = np.sign(state)
         mid_sum = (self.size + self.size - 2) // 2
         print("            1 A")
         for sum in range(self.size + self.size - 1):
@@ -142,10 +146,7 @@ class Hex:
                 j = sum - i
                 if j >= self.size:
                     continue
-                if blob:
-                    print(int(state[j, i]), end=" ")
-                else:
-                    print(no_to_symbol[state[j, i]], end=" ")
+                print(int(state[j, i]), end=" ")
             if sum < self.size - 1:
                 print(f"{chr(sum + 66)}")
             else:
@@ -171,10 +172,34 @@ class Hex:
         col = chr(65 + col)
         return col + str(row)
 
-
+    def debug_game(self, moves):
+        import os
+        state = self.get_initial_state()
+        player = 1
+        for move in moves:
+            _ = input("Enter for next move...")
+            os.system('clear')
+            action = self.str_to_action(move)
+            if not 0 <= action < self.action_size or not self.get_valid_moves(state)[action]:
+                print(f"[ERR]: Invalid move {move} (action: {action})")
+            else:
+                print(f"{player} played {move}")
+            state = self.get_next_state(state, action, player)
+            print(self.get_value_and_terminated(state, action))
+            self.pretty_please_state(state)
+            player *= -1
 
 if __name__ == "__main__":
+    moves = open("test.txt").read().splitlines()
     hex = Hex()
-    state = hex.get_initial_state()
-    state = hex.get_next_state(state, hex.str_to_action("B3"), 1)
+    # hex.debug_game(moves)
+
+    state = np.zeros((11, 11))
+    state[:, 0] = 1
     hex.pretty_please_state(state)
+    print(hex.check_win(state, 0))
+    state = np.zeros((11, 11))
+    state[3, :] = -1
+    state[3, -1] = 1
+    hex.pretty_please_state(state)
+    print(hex.check_win(state, 33))
